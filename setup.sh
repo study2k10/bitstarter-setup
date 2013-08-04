0ee5737ea9e7bf84fe4e4e49bee45c228a73b627
Andy <study2k10+github@gmail.com>
2013-08-04 17:53:59 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,11 +21,8 @@ npm install -g jshint
 # See: http://nodejs.org/api/repl.html#repl_repl
 sudo apt-get install -y rlwrap
 
-# Install emacs24
-# https://launchpad.net/~cassou/+archive/emacs
-sudo apt-add-repository -y ppa:cassou/emacs
-sudo apt-get -qq update
-sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
+# Install some other nice goodies.
+sudo apt-get install -y htop
 
 # Install Heroku toolbelt
 # https://toolbelt.heroku.com/debian
@@ -36,9 +33,7 @@ cd $HOME
 if [ -d ./dotfiles/ ]; then
     mv dotfiles dotfiles.old
 fi
-if [ -d .emacs.d/ ]; then
-    mv .emacs.d .emacs.d~
-fi
+
 git clone https://github.com/startup-class/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
